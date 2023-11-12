983c298720674a3e7983297b32ca5ceb6603832c
criszav <czavala.dev@gmail.com>
2023-11-12 14:51:10 -0300

    add not found exception

---
+++ src/main/java/com/czavala/springsecurityjwt/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,16 @@
+package com.czavala.springsecurityjwt.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException() {
+        super();
+    }
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
